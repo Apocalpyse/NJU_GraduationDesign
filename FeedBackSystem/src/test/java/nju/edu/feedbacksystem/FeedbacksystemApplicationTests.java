ba927ea15dafd3512aedb6b225d3963243038654
delong chang <2578931175@qq.com>
2019-04-20 15:15:01 +0800

    反馈系统创建

---
+++ FeedBackSystem/src/test/java/nju/edu/feedbacksystem/FeedbacksystemApplicationTests.java
@@ -0,0 +1,16 @@
+package nju.edu.feedbacksystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedbacksystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
